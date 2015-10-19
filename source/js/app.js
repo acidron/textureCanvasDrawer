ccc962d0816d35f109a2afdb96bc069ad2775b18
Nakhabov Andrei <dron@MacBook-Pro.local>
2015-10-19 18:34:58 +0300

    + access data from outside the plugin

--- source/js/app.js
+++ source/js/app.js
@@ -1,11 +1,19 @@
-(function () {
-	$.fn.drawer = function() {
-		var data = [];
+(function ($) {
+	$.fn.drawer = function(options) {
+		if (options != undefined) {
+			switch (options) {
+				case 'getData':
+					return this.data;
+			}
+		}
+		var $this = this;
+		$this.data = [];
+
 		var canvas = $('<canvas>');
 		canvas.click(function(e) {
 			var c = canvas.get(0).getContext('2d');
 			c.fillRect(e.offsetX, e.offsetY, 5, 5);
-			data.push({x: e.offsetX, y: e.offsetY});
+			$this.data.push({x: e.offsetX, y: e.offsetY});
 		});
 
 		canvas.prop({
@@ -13,5 +21,7 @@
 			height: this.height()
 		});
 		this.html('').append(canvas);
+
+		return this;
 	};
-})();
+})(jQuery);
