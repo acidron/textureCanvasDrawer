f40a7cdb9d13141599be3361ea4b9099a9d396af
Nakhabov Andrei <dron@MacBook-Pro.local>
2015-10-19 18:05:26 +0300

    + click to point

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,15 @@
-$.fn.drawer = function() {
-	
-};
+(function () {
+	$.fn.drawer = function() {
+		var canvas = $('<canvas>');
+		canvas.click(function(e) {
+			var c = canvas.get(0).getContext('2d');
+			c.fillRect(e.offsetX, e.offsetY, 5, 5);
+		});
+
+		canvas.prop({
+			width: this.width(),
+			height: this.height()
+		});
+		this.html('').append(canvas);
+	};
+})();
