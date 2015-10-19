0971d9fc7b7c7d0b516bf5c62586ac87545ad77d
Nakhabov Andrei <dron@MacBook-Pro.local>
2015-10-19 17:37:01 +0300

    blank app

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+//require('es6-promise').polyfill();
+var gulp   = require('gulp'),
+    concat = require('gulp-concat');
+
+var browserSync = null;
+
+gulp.task("js", function() {
+	var stream = gulp.src("source/js/*.js")
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest("build/"));
+	if (browserSync) {
+		stream.pipe(browserSync.reload());
+	}
+	return stream;
+});
+
+gulp.task("watch", ["js"], function() {
+	browserSync = require('browser-sync').create();
+    browserSync.init({
+        open: false,
+        notify: false,
+        reload: true,
+        proxy: '192.168.33.10/latista'
+    });
+	gulp.watch("source/js/*.js", ["js"]);
+});
+
+gulp.task('default', [ "watch"]);
