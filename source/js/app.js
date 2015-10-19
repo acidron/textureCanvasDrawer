bcacb1727079dd3978f15600ff8bf69c386e35a4
Nakhabov Andrei <dron@MacBook-Pro.local>
2015-10-19 18:11:52 +0300

    + save points to array

--- source/js/app.js
+++ source/js/app.js
@@ -1,9 +1,11 @@
 (function () {
 	$.fn.drawer = function() {
+		var data = [];
 		var canvas = $('<canvas>');
 		canvas.click(function(e) {
 			var c = canvas.get(0).getContext('2d');
 			c.fillRect(e.offsetX, e.offsetY, 5, 5);
+			data.push({x: e.offsetX, y: e.offsetY});
 		});
 
 		canvas.prop({
