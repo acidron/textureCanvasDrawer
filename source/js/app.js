48af4a38c8fa14155402e8368a87158571fd6caf
Nakhabov Andrei <dron@MacBook-Pro.local>
2015-10-21 11:16:11 +0300

    + proof-of-concept of a deferred rendering

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,8 @@
 (function ($) {
 	var Config = {
 		resolution: 50,
-		sameDelta: 10
+		sameDelta: 10,
+		instantRender: true
 	};
 
 	$.fn.drawer = function(options) {
@@ -12,9 +13,46 @@
 			}
 		}
 
+		/*
+		 * Data
+		 */
+		var $this = this;
+		$this.data = [];
+		$this.tool = 'line';
+		/*
+		 * Methods
+		 */
 		var actions = {
-			drawPoint: function(e) {
-				var point = {x: e.offsetX, y: e.offsetY};
+			selectTool: function(tool) {
+				$this.tool = tool;
+			},
+			render: function() {
+				var c = canvas.get(0);
+				var context = c.getContext('2d');
+				context.fillStyle="#FFFFFF";
+				context.fillRect(0, 0, c.width, c.height);
+				context.fillStyle="#000000";
+				c = context;
+				var prevPoint = null;
+				_.each($this.data, function(point) {
+					context.fillRect(point.x, point.y, 5, 5);	
+					if (point.type == 'line') {
+						if (prevPoint !== null) {
+							c.beginPath();
+							c.moveTo(prevPoint.x, prevPoint.y);
+							c.lineTo(point.x, point.y);
+							c.stroke();
+						}
+					} 
+					if (point.type == 'rect') {
+						context.strokeRect(point.x, point.y, prevPoint.x - point.x, prevPoint.y - point.y);
+					}
+					prevPoint = point;
+				});
+				
+			},
+			drawPoint: function(e, type) {
+				var point = {x: e.offsetX, y: e.offsetY, type: type};
 
 				var lastSaved = _.last($this.data);
 				if (lastSaved != undefined) {
@@ -22,15 +60,19 @@
 						return null;
 					}
 				}
-
-				var c = canvas.get(0).getContext('2d');
-				c.fillRect(e.offsetX, e.offsetY, 5, 5);
 				$this.data.push(point);
+
+				if (Config.instantRender) {
+					var c = canvas.get(0).getContext('2d');
+					c.fillRect(e.offsetX, e.offsetY, 5, 5);	
+				}
 				return point;
 			},
 			trackMouseMoving: function(e) {
+				if ($this.tool == 'rect') return;
 				var prevPoint = _.last($this.data);
-				if (actions.drawPoint(e) !== null) {
+				if (prevPoint.type == 'point') prevPoint.type = 'startline';
+				if (actions.drawPoint(e, 'line') !== null) {
 					var c = canvas.get(0).getContext('2d');
 					c.beginPath();
 					c.moveTo(prevPoint.x, prevPoint.y);
@@ -38,32 +80,45 @@
 					c.stroke();
 					isDrawing = true;
 				}
+			},
+			trackMouseEndMoving: function(e) {
+				canvas.off('mousemove', actions.trackMouseMovingThrottled);
+				var prevPoint = _.last($this.data);
+				var point = actions.drawPoint(e, isDrawing ? 'line' : 'point');
+				isDrawing = false;
+
+				if ($this.tool == 'rect') {
+					point.type = 'rect';
+					actions.render();
+				}
 			}
 		};
 
 		actions.trackMouseMovingThrottled = _.throttle(actions.trackMouseMoving, Config.resolution);
 
-		var $this = this;
-		$this.data = [];
-
 		var canvas = $('<canvas>');
 
 		var isDrawing = false;
 		canvas.on('mousedown', function(e) {
-			actions.drawPoint(e);
+			actions.drawPoint(e, 'point');
 			canvas.on('mousemove', actions.trackMouseMovingThrottled);
 		});
-		canvas.on('mouseup', function(e) {
-			canvas.off('mousemove', actions.trackMouseMovingThrottled);
-			actions.drawPoint(e);
-			isDrawing = false;
-		});
+		canvas.on('mouseup', actions.trackMouseEndMoving);
 
 		canvas.prop({
 			width: this.width(),
 			height: this.height()
 		});
-		this.html('').append(canvas);
+		var toolbar = $('<div>');
+		toolbar.css({
+			position: 'absolute',
+			top: '-20px',
+			right: 0
+		});
+		toolbar.append($('<button>Line</button>').click(function() {actions.selectTool('line');}));
+		toolbar.append($('<button>Rect</button>').click(function() {actions.selectTool('rect');}));
+		toolbar.append($('<button>Render</button>').click(actions.render));
+		this.html('').css({position: 'relative'}).append(toolbar).append(canvas);
 
 		return this;
 	};
